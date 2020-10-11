dc3b84a56f5c8e34d2ff5be3f95392de926344a4
ShivamPrashantBagade <69910046+ShivamPrashantBagade@users.noreply.github.com>
2020-10-11 22:09:48 +0530

    Update MathsTable.py

--- MathsTable.py
+++ MathsTable.py
@@ -1,7 +1,11 @@
-def MathsTable(num, end_point):
+def MathsTable():
 
-    for n in range(1, end_point+1):
-        print(f"{num} x {n} = {n*num}")
+    user_input1 = int(input("Table of :"))
+    user_input2 = int(input("Till number :"))
+
+    for n in range(1, user_input2+1):
+
+        print(f"{user_input1} x {n} = {n*user_input1}")
 
 
-MathsTable(5, 10)
+MathsTable()
